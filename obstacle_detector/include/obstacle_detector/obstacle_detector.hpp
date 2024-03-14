1e23e58da04f5ea4dd545d57e517522c24f5f317
So-Nakanishi <ee213124@meiji.ac.jp>
2024-03-14 15:22:32 +0900

    obstacle commit

---
+++ obstacle_detector/include/obstacle_detector/obstacle_detector.hpp
@@ -0,0 +1,38 @@
+#ifndef OBSTACLR_DETECTOE_HPP
+#define OBSTACLR_DETECTOE_HPP
+
+#include <rclcpp/rclcpp.hpp>
+#include <functional>
+#include <memory>
+#include <sensor_msgs/msg/laser_scan.hpp>
+#include <geometry_msgs/msg/pose_array.hpp>
+#include <geometry_msgs/msg/pose.hpp>
+
+class ObstacleDetector : public rclcpp::Node
+{
+    public:
+        ObstacleDetector();     // コンストラクタ
+        void process();
+        int getFreq();  // hz_を返す関数
+    
+    private:
+        void laser_scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg);
+        void scan_obstacle();
+        bool is_ignore_scan(double angle);
+
+        int hz_;
+        int laser_step_;
+        double ignore_distance_;
+        std::string robot_frame_;
+        std::vector<double> ignore_angle_range_list_;
+
+        bool flag_laser_scan_ = false;
+
+        rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_scan_sub_;
+        rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr obstacle_pose_pub_;
+
+        geometry_msgs::msg::PoseArray obstacle_pose_array_;
+        sensor_msgs::msg::LaserScan laser_scan_;
+};
+
+#endif // OBSTACLR_DETECTOE_H
